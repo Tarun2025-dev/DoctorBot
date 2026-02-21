e0142ce8d856b2fbdba90f5c1722c737a558df4d
Tarun2025-dev <vtarunvikas@gmail.com>
2026-02-21 18:09:46 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="doctor-bot",
+    version="0.1.0",
+    author="Tarun Vikas",
+    author_email="vtarunvikas@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
